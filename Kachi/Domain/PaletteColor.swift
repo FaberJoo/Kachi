39bdd3f8ea155153d35689ba13d3e8ff66425f2e
FaberJoo <sjdk0805@gmail.com>
2026-04-21 22:27:45 +0900

    feat: Add new color palettes for implement PaletteColor enum in SwiftUI #1

---
+++ Kachi/Domain/PaletteColor.swift
@@ -0,0 +1,164 @@
+import SwiftUI
+
+enum PaletteColor {
+
+    enum Gray {
+        static let c00  = Color("Gray000")
+        static let c05  = Color("Gray005")
+        static let c10  = Color("Gray010")
+        static let c20  = Color("Gray020")
+        static let c30  = Color("Gray030")
+        static let c40  = Color("Gray040")
+        static let c50  = Color("Gray050")
+        static let c60  = Color("Gray060")
+        static let c70  = Color("Gray070")
+        static let c80  = Color("Gray080")
+        static let c90  = Color("Gray090")
+        static let c95  = Color("Gray095")
+        static let c100 = Color("Gray100")
+    }
+
+    enum Red {
+        static let c00  = Color("Red000")
+        static let c05  = Color("Red005")
+        static let c10  = Color("Red010")
+        static let c20  = Color("Red020")
+        static let c30  = Color("Red030")
+        static let c40  = Color("Red040")
+        static let c50  = Color("Red050")
+        static let c60  = Color("Red060")
+        static let c70  = Color("Red070")
+        static let c80  = Color("Red080")
+        static let c90  = Color("Red090")
+        static let c95  = Color("Red095")
+        static let c100 = Color("Red100")
+    }
+
+    enum Brown {
+        static let c00  = Color("Brown000")
+        static let c05  = Color("Brown005")
+        static let c10  = Color("Brown010")
+        static let c20  = Color("Brown020")
+        static let c30  = Color("Brown030")
+        static let c40  = Color("Brown040")
+        static let c50  = Color("Brown050")
+        static let c60  = Color("Brown060")
+        static let c70  = Color("Brown070")
+        static let c80  = Color("Brown080")
+        static let c90  = Color("Brown090")
+        static let c95  = Color("Brown095")
+        static let c100 = Color("Brown100")
+    }
+
+    enum Orange {
+        static let c00  = Color("Orange000")
+        static let c05  = Color("Orange005")
+        static let c10  = Color("Orange010")
+        static let c20  = Color("Orange020")
+        static let c30  = Color("Orange030")
+        static let c40  = Color("Orange040")
+        static let c50  = Color("Orange050")
+        static let c60  = Color("Orange060")
+        static let c70  = Color("Orange070")
+        static let c80  = Color("Orange080")
+        static let c90  = Color("Orange090")
+        static let c95  = Color("Orange095")
+        static let c100 = Color("Orange100")
+    }
+
+    enum Yellow {
+        static let c00  = Color("Yellow000")
+        static let c05  = Color("Yellow005")
+        static let c10  = Color("Yellow010")
+        static let c20  = Color("Yellow020")
+        static let c30  = Color("Yellow030")
+        static let c40  = Color("Yellow040")
+        static let c50  = Color("Yellow050")
+        static let c60  = Color("Yellow060")
+        static let c70  = Color("Yellow070")
+        static let c80  = Color("Yellow080")
+        static let c90  = Color("Yellow090")
+        static let c95  = Color("Yellow095")
+        static let c100 = Color("Yellow100")
+    }
+
+    enum Green {
+        static let c00  = Color("Green000")
+        static let c05  = Color("Green005")
+        static let c10  = Color("Green010")
+        static let c20  = Color("Green020")
+        static let c30  = Color("Green030")
+        static let c40  = Color("Green040")
+        static let c50  = Color("Green050")
+        static let c60  = Color("Green060")
+        static let c70  = Color("Green070")
+        static let c80  = Color("Green080")
+        static let c90  = Color("Green090")
+        static let c95  = Color("Green095")
+        static let c100 = Color("Green100")
+    }
+
+    enum Cyan {
+        static let c00  = Color("Cyan000")
+        static let c05  = Color("Cyan005")
+        static let c10  = Color("Cyan010")
+        static let c20  = Color("Cyan020")
+        static let c30  = Color("Cyan030")
+        static let c40  = Color("Cyan040")
+        static let c50  = Color("Cyan050")
+        static let c60  = Color("Cyan060")
+        static let c70  = Color("Cyan070")
+        static let c80  = Color("Cyan080")
+        static let c90  = Color("Cyan090")
+        static let c95  = Color("Cyan095")
+        static let c100 = Color("Cyan100")
+    }
+
+    enum Blue {
+        static let c00  = Color("Blue000")
+        static let c05  = Color("Blue005")
+        static let c10  = Color("Blue010")
+        static let c20  = Color("Blue020")
+        static let c30  = Color("Blue030")
+        static let c40  = Color("Blue040")
+        static let c50  = Color("Blue050")
+        static let c60  = Color("Blue060")
+        static let c70  = Color("Blue070")
+        static let c80  = Color("Blue080")
+        static let c90  = Color("Blue090")
+        static let c95  = Color("Blue095")
+        static let c100 = Color("Blue100")
+    }
+
+    enum Purple {
+        static let c00  = Color("Purple000")
+        static let c05  = Color("Purple005")
+        static let c10  = Color("Purple010")
+        static let c20  = Color("Purple020")
+        static let c30  = Color("Purple030")
+        static let c40  = Color("Purple040")
+        static let c50  = Color("Purple050")
+        static let c60  = Color("Purple060")
+        static let c70  = Color("Purple070")
+        static let c80  = Color("Purple080")
+        static let c90  = Color("Purple090")
+        static let c95  = Color("Purple095")
+        static let c100 = Color("Purple100")
+    }
+
+    enum Pink {
+        static let c00  = Color("Pink000")
+        static let c05  = Color("Pink005")
+        static let c10  = Color("Pink010")
+        static let c20  = Color("Pink020")
+        static let c30  = Color("Pink030")
+        static let c40  = Color("Pink040")
+        static let c50  = Color("Pink050")
+        static let c60  = Color("Pink060")
+        static let c70  = Color("Pink070")
+        static let c80  = Color("Pink080")
+        static let c90  = Color("Pink090")
+        static let c95  = Color("Pink095")
+        static let c100 = Color("Pink100")
+    }
+}
